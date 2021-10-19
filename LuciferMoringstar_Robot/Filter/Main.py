c7b98507c9f738cafe80f882f834c124e158c359
scurire <92737553+scurire@users.noreply.github.com>
2021-10-19 13:34:27 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -277,7 +277,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "help":
             buttons = [
                 [
-                    InlineKeyboardButton('⚜️ Join Our Channel ⚜️', url=f'')
+                    InlineKeyboardButton('⚜️ Join Our Channel ⚜️', url=f'{TUTORIAL}')
                 ]
                 ]
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('⚜️ Join Our Channel ⚜️', url=f'')
+                    InlineKeyboardButton('⚜️ Join Our Channel ⚜️', url=f'{TUTORIAL}')
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -308,7 +308,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('⚜️ Join Channel ⚜️', url=f'')
+                        InlineKeyboardButton('⚜️ Join Channel ⚜️', url=f'{TUTORIAL}')
                     ]
                     ]
                 
@@ -339,7 +339,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('⚜️ Join Channel ⚜️, url=f'')
+                        InlineKeyboardButton('⚜️ Join Channel ⚜️, url=f'{TUTORIAL}')
                     ]
                     ]
                 
