731bb25d9436721db6ec527d3a4b76aef11a2edf
scurire <92737553+scurire@users.noreply.github.com>
2021-10-19 10:18:33 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -277,7 +277,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "help":
             buttons = [
                 [
-                    InlineKeyboardButton('⚜️ Join Channel ⚜️', url='https://t.me/Mollywood_channel')
+                    InlineKeyboardButton('⚜️ Join Our Channel ⚜️', url='https://t.me/Mollywood_channel')
                 ]
                 ]
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('Making Video', url=f'{TUTORIAL}')
+                    InlineKeyboardButton('⚜️ Join Our Channel ⚜️', url='https://t.me/Mollywood_channel')
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
